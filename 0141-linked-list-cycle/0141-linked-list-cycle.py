bf8e8d68a6e18f2135021f99fa86389d5533f163
dhvanichheda <dhvani.chheda@intel.com>
2024-08-28 11:57:14 -0700

    Time: 44 ms (64.73%), Space: 19.1 MB (62.65%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -1,5 +1,6 @@
 '''
 1. Store each node in a hash map and check if it appears again
+2. Fast and slow runner 
 '''
 # Definition for singly-linked list.
 # class ListNode:
@@ -7,6 +8,8 @@
 #         self.val = x
 #         self.next = None
 
+'''
+#Approach 1
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
         node_set = set()
@@ -19,3 +22,22 @@ class Solution:
                 head = head.next
 
         return False 
+'''
+
+#Approach 2 
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        if head is None:
+            return False
+
+        slow = head
+        fast = head.next
+
+        while fast != slow:
+            if fast is None or fast.next is None:
+                return False
+            
+            slow = slow.next
+            fast = fast.next.next
+
+        return True 
