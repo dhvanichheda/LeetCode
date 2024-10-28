ad50148e5308e8cdcd502f22d4548e77065f2679
dhvanichheda <dhvanichheda93@gmail.com>
2024-10-27 19:19:33 -0700

    Time: 0 ms (100%), Space: 16.6 MB (69.34%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.py
+++ 0541-reverse-string-ii/0541-reverse-string-ii.py
@@ -2,7 +2,7 @@ class Solution:
     def reverseStr(self, s: str, k: int) -> str:
         a = list(s)
         for i in range(0, len(a), 2*k):
-            a[i:i+k] = reversed(a[i:i+k])
+            a[i:i+k] = a[i:i+k][::-1] # Python slicing handles out-of-range indices gracefully. So if i+k is beyond the string length, it will only consider the slice up to the end of the string 
 
         return "".join(a)
 
