1f309a1e178274d73b046a4814832957d7c0bc5f
dhvanichheda <dhvanichheda93@gmail.com>
2024-11-05 17:44:56 -0800

    Time: 4 ms (91.06%), Space: 17 MB (80.32%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,22 +1,17 @@
-from string import ascii_letters, digits
-
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-    
-        #convert all to lowercase and remove all non-alphanumeric characters
-        s = "".join([c for c in s if c in (ascii_letters + digits)])
-        s = s.lower()
-        if (len(s) == 1) or s == "":
-            return True 
- 
-        #start traversing from front and back and compare. Stop at the mid point 
-        start_idx = 0 
-        end_idx = len(s) - 1
-        while start_idx < end_idx:
-            if s[start_idx] != s[end_idx]:
+        clean_phrase = ""
+        for c in s.lower():
+            if c.isalnum():
+                clean_phrase += c
+
+        i, j = 0, len(clean_phrase) - 1
+        while i < j:
+            if clean_phrase[i] != clean_phrase[j]:
                 return False 
-            start_idx += 1
-            end_idx -= 1
+
+            i += 1
+            j -= 1
 
         return True
-
+        
